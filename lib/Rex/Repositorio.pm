10b9d0285570a4a3729a62da88dbe773bc2ecea5
Jan Gehring <cpan@imail.com>
2014-03-15 08:21:01 +0000

    Import of JFRIED/Rex-Repositorio-0.0.1 from CPAN.
    gitpan-cpan-distribution: Rex-Repositorio
    gitpan-cpan-version:      0.0.1
    gitpan-cpan-path:         JFRIED/Rex-Repositorio-0.0.1.tar.gz
    gitpan-cpan-author:       JFRIED
    gitpan-cpan-maturity:     released

---
+++ lib/Rex/Repositorio.pm
@@ -0,0 +1,541 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+#
+# vim: set ts=2 sw=2 tw=0:
+# vim: set expandtab:
+
+package Rex::Repositorio;
+
+use Moose;
+use English;
+use common::sense;
+use Carp;
+use LWP::UserAgent;
+use XML::LibXML;
+use XML::Simple;
+use Params::Validate qw(:all);
+use IO::All;
+use File::Path;
+use File::Basename qw'dirname';
+use File::Spec;
+use File::Copy;
+use Rex::Repositorio::Repository_Factory;
+
+our $VERSION = "0.0.1";
+
+has config => ( is => 'ro' );
+has logger => ( is => 'ro' );
+has ua     => (
+  is      => 'ro',
+  default => sub {
+    my $ua = LWP::UserAgent->new;
+    $ua->env_proxy;
+    return $ua;
+  }
+);
+
+sub run {
+  my ( $self, %option ) = @_;
+
+  $self->config->{RepositoryRoot} =~ s/\/$//;
+  $self->parse_cli_option(%option);
+}
+
+sub parse_cli_option {
+  my ( $self, %option ) = @_;
+
+  if ( exists $option{help} ) {
+    $self->_help();
+    exit 0;
+  }
+
+  if ( exists $option{mirror} && exists $option{repo} ) {
+    $self->mirror(
+      repo            => $option{repo},
+      update_metadata => $option{"update-metadata"},
+      update_files    => $option{"update-files"},
+    );
+  }
+
+  elsif ( exists $option{tag} && exists $option{repo} ) {
+    $self->tag( tag => $option{tag}, repo => $option{repo} );
+  }
+
+  elsif ( exists $option{list} ) {
+    $self->list();
+  }
+
+  elsif ( exists $option{init} && exists $option{repo} ) {
+    $self->init( repo => $option{repo} );
+  }
+
+  elsif ( exists $option{"add-file"} && exists $option{repo} ) {
+    $self->add_file( file => $option{"add-file"}, repo => $option{repo} );
+  }
+
+  elsif ( exists $option{"remove-file"} && exists $option{repo} ) {
+    $self->remove_file( file => $option{"remove-file"}, repo => $option{repo} );
+  }
+
+  else {
+    $self->_help();
+    exit 0;
+  }
+}
+
+sub add_file {
+  my $self   = shift;
+  my %option = validate(
+    @_,
+    {
+      file => {
+        type => SCALAR
+      },
+      repo => {
+        type => SCALAR
+      }
+    }
+  );
+
+  my $repo   = $self->config->{Repository}->{ $option{repo} };
+  my $type   = $repo->{type};
+  my $repo_o = Rex::Repositorio::Repository_Factory->create(
+    type    => $type,
+    options => {
+      app  => $self,
+      repo => {
+        name => $option{repo},
+        %{$repo},
+      }
+    }
+  );
+
+  $repo_o->add_file( file => $option{file} );
+}
+
+sub remove_file {
+  my $self   = shift;
+  my %option = validate(
+    @_,
+    {
+      file => {
+        type => SCALAR
+      },
+      repo => {
+        type => SCALAR
+      }
+    }
+  );
+
+  my $repo   = $self->config->{Repository}->{ $option{repo} };
+  my $type   = $repo->{type};
+  my $repo_o = Rex::Repositorio::Repository_Factory->create(
+    type    => $type,
+    options => {
+      app  => $self,
+      repo => {
+        name => $option{repo},
+        %{$repo},
+      }
+    }
+  );
+
+  $repo_o->remove_file( file => $option{file} );
+}
+
+sub list {
+  my $self  = shift;
+  my @repos = keys %{ $self->config->{Repository} };
+
+  $self->_print(@repos);
+}
+
+sub init {
+  my $self   = shift;
+  my %option = validate(
+    @_,
+    {
+      repo => {
+        type => SCALAR
+      }
+    }
+  );
+
+  my $repo = $self->config->{Repository}->{ $option{repo} };
+
+  if ( !$repo ) {
+    $self->logger->error("Repository $option{repo} not found.");
+    confess "Repository $option{repo} not found.";
+  }
+
+  my $type   = $repo->{type};
+  my $repo_o = Rex::Repositorio::Repository_Factory->create(
+    type    => $type,
+    options => {
+      app  => $self,
+      repo => {
+        name => $option{repo},
+        %{$repo},
+      }
+    }
+  );
+
+  $repo_o->init;
+}
+
+sub mirror {
+  my $self   = shift;
+  my %option = validate(
+    @_,
+    {
+      repo => {
+        type => SCALAR
+      },
+      update_metadata => {
+        type     => BOOLEAN,
+        optional => 1,
+      },
+      update_files => {
+        type     => BOOLEAN,
+        optional => 1,
+      },
+    }
+  );
+
+  my @repositories = ( $option{repo} );
+  if ( $option{repo} eq "all" ) {
+    @repositories = keys %{ $self->config->{Repository} };
+  }
+
+  for my $repo (@repositories) {
+    my $type = $self->config->{Repository}->{$repo}->{type};
+
+    my $repo_o = Rex::Repositorio::Repository_Factory->create(
+      type    => $type,
+      options => {
+        app  => $self,
+        repo => {
+          name => $repo,
+          %{ $self->config->{Repository}->{$repo} },
+        }
+      }
+    );
+
+    $repo_o->mirror(
+      update_metadata => $option{update_metadata},
+      update_files    => $option{update_files}
+    );
+  }
+}
+
+sub tag {
+  my $self   = shift;
+  my %option = validate(
+    @_,
+    {
+      repo => {
+        type => SCALAR
+      },
+      tag => {
+        type => SCALAR
+      },
+    }
+  );
+
+  my $repo_config = $self->config->{Repository}->{ $option{repo} };
+  my $root_dir    = $self->config->{RepositoryRoot};
+  $repo_config->{local} =~ s/\/$//;
+
+  my @dirs    = ("$root_dir/head/$repo_config->{local}");
+  my $tag_dir = "$root_dir/$option{tag}/$repo_config->{local}";
+
+  mkpath $tag_dir;
+
+  for my $dir (@dirs) {
+    opendir my $dh, $dir;
+    while ( my $entry = readdir $dh ) {
+      next if ( $entry eq "." || $entry eq ".." );
+      my $rel_entry = "$dir/$entry";
+      $rel_entry =~ s/$root_dir\/head\/$repo_config->{local}\///;
+
+      if ( -d "$dir/$entry" ) {
+        push @dirs, "$dir/$entry";
+        $self->logger->debug("Creating directory: $tag_dir/$rel_entry.");
+        mkdir "$tag_dir/$rel_entry";
+        next;
+      }
+
+      $self->logger->debug(
+        "Linking (hard): $dir/$entry -> $tag_dir/$rel_entry");
+      link "$dir/$entry", "$tag_dir/$rel_entry";
+    }
+    closedir $dh;
+  }
+}
+
+sub download_gzip {
+  my ( $self, $url ) = @_;
+
+  my $content = $self->download($url);
+
+  require Compress::Zlib;
+
+  $self->logger->debug("Starting uncompressing of: $url");
+
+  my $un_content = Compress::Zlib::memGunzip($content);
+  $self->logger->debug("Finished uncompressing of: $url");
+  if ( !$un_content ) {
+    $self->logger->error("Error uncompressing data.");
+    confess "Error uncompressing data.";
+  }
+
+  return $un_content;
+}
+
+sub download {
+  my ( $self, $url ) = @_;
+
+  $self->logger->debug("Starting download of: $url");
+  my $resp = $self->ua->get($url);
+  $self->logger->debug("Finished download of: $url");
+
+  if ( !$resp->is_success ) {
+    $self->logger->error("Can't download $url.");
+    $self->logger->error( "Status: " . $resp->status_line );
+    confess "Error downloading $url.";
+  }
+
+  return $resp->content;
+}
+
+sub get_xml {
+  my ( $self, $xml ) = @_;
+  return XML::LibXML->load_xml( string => $xml );
+}
+
+sub decode_xml {
+  my ( $self, $xml ) = @_;
+  return XMLin( $xml, ForceArray => 1 );
+}
+
+sub download_package {
+  my $self   = shift;
+  my %option = validate(
+    @_,
+    {
+      name => {
+        type => SCALAR
+      },
+      url => {
+        type => SCALAR
+      },
+      dest => {
+        type => SCALAR
+      },
+      cb => {
+        type     => CODEREF,
+        optional => 1,
+      },
+      force => {
+        type     => BOOLEAN,
+        optional => 1,
+      }
+    }
+  );
+
+  my $package_file = $self->config->{RepositoryRoot} . "/head/" . $option{dest};
+  $self->_download_binary_file(
+    dest  => $package_file,
+    url   => $option{url},
+    cb    => $option{cb},
+    force => $option{force},
+  );
+}
+
+sub download_metadata {
+  my $self   = shift;
+  my %option = validate(
+    @_,
+    {
+      url => {
+        type => SCALAR
+      },
+      dest => {
+        type => SCALAR
+      },
+      cb => {
+        type     => CODEREF,
+        optional => 1,
+      },
+      force => {
+        type     => BOOLEAN,
+        optional => 1,
+      }
+    }
+  );
+
+  my $metadata_file =
+    $self->config->{RepositoryRoot} . "/head/" . $option{dest};
+  $self->_download_binary_file(
+    dest  => $metadata_file,
+    url   => $option{url},
+    cb    => $option{cb},
+    force => $option{force},
+  );
+}
+
+sub _download_binary_file {
+  my $self   = shift;
+  my %option = validate(
+    @_,
+    {
+      url => {
+        type => SCALAR
+      },
+      dest => {
+        type => SCALAR
+      },
+      cb => {
+        type     => CODEREF | UNDEF,
+        optional => 1,
+      },
+      force => {
+        type => BOOLEAN
+      }
+    }
+  );
+
+  $self->logger->debug("Downloading: $option{url} -> $option{dest}");
+
+  mkpath( dirname( $option{dest} ) ) if ( !-d dirname $option{dest} );
+
+  if ( -f $option{dest} && !$option{force} ) {
+    $self->logger->debug("Skipping $option{url}. File aready exists.");
+    return;
+  }
+
+  if ( !-w dirname( $option{dest} ) ) {
+    $self->logger->error( "Can't write to " . dirname( $option{dest} ) );
+    confess "Can't write to " . dirname( $option{dest} );
+  }
+
+  if ( -f $option{dest} && $option{force} ) {
+    unlink $option{dest};
+  }
+
+  open my $fh, ">", $option{dest};
+  binmode $fh;
+  my $resp = $self->ua->get(
+    $option{url},
+    ':content_cb' => sub {
+      my ( $data, $response, $protocol ) = @_;
+      print $fh $data;
+    }
+  );
+  close $fh;
+
+  if ( !$resp->is_success ) {
+    $self->logger->error("Can't download $option{url}.");
+    $self->logger->error( "Status: " . $resp->status_line );
+    confess "Error downloading $option{url}.";
+  }
+
+  $option{cb}->( $option{dest} ) if ( exists $option{cb} && $option{cb} );
+}
+
+sub get_repo_dir {
+  my $self   = shift;
+  my %option = validate(
+    @_,
+    {
+      repo => {
+        type => SCALAR
+      }
+    }
+  );
+
+  return File::Spec->rel2abs( $self->config->{RepositoryRoot}
+      . "/head/"
+      . $self->config->{Repository}->{ $option{repo} }->{local} );
+}
+
+sub add_file_to_repo {
+  my $self   = shift;
+  my %option = validate(
+    @_,
+    {
+      source => {
+        type => SCALAR
+      },
+      dest => {
+        type => SCALAR
+      }
+    }
+  );
+
+  if ( !-f $option{source} ) {
+    $self->logger->error("Fild $option{source} not found.");
+    confess "Fild $option{source} not found.";
+  }
+
+  $self->logger->debug("Copy $option{source} -> $option{dest}");
+  my $ret = copy $option{source}, $option{dest};
+  if ( !$ret ) {
+    $self->logger->error("Error copying file $option{source} to $option{dest}");
+    confess "Error copying file $option{source} to $option{dest}";
+  }
+}
+
+sub remove_file_from_repo {
+  my $self   = shift;
+  my %option = validate(
+    @_,
+    {
+      file => {
+        type => SCALAR
+      }
+    }
+  );
+
+  if ( !-f $option{file} ) {
+    $self->logger->error("Fild $option{file} not found.");
+    confess "Fild $option{file} not found.";
+  }
+
+  $self->logger->debug("Deleting $option{file}.");
+  my $ret = unlink $option{file};
+  if ( !$ret ) {
+    $self->logger->error("Error deleting file $option{file}");
+    confess "Error deleting file $option{file}";
+  }
+}
+
+sub _print {
+  my $self  = shift;
+  my @lines = @_;
+
+  print "repositorio: $VERSION\n";
+  print "-" x 80;
+  print "\n";
+  print "$_\n" for @lines;
+}
+
+sub _help {
+  my ($self) = @_;
+
+  $self->_print(
+    "--mirror            mirror a configured repository (needs --repo)",
+    "--tag=tagname       tag a repository (needs --repo)",
+    "--repo=reponame     the name of the repository to use",
+    "--update-metadata   update the metadata of a repository",
+    "--update-files      download files even if they are already downloaded",
+    "--init              initialize an empty repository",
+    "--add-file=file     add a file to a repository (needs --repo)",
+    "--remove-file=file  remove a file from a repository (needs --repo)",
+    "--list              list known repositories",
+    "--help              display this help message",
+  );
+
+}
+
+1;
---
+++ lib/Rex/Repositorio/Repository/Yum.pm
@@ -0,0 +1,211 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+#
+# vim: set ts=2 sw=2 tw=0:
+# vim: set expandtab:
+
+package Rex::Repositorio::Repository::Yum;
+
+use Moose;
+use Try::Tiny;
+use File::Basename qw'basename';
+use Data::Dumper;
+use Digest::SHA;
+use Carp;
+use Params::Validate qw(:all);
+use File::Spec;
+use File::Path;
+
+extends "Rex::Repositorio::Repository::Base";
+
+sub mirror {
+  my ( $self, %option ) = @_;
+
+  $self->repo->{url} =~ s/\/$//;
+  $self->repo->{local} =~ s/\/$//;
+  my $name = $self->repo->{name};
+
+  my $repomd_ref =
+    $self->app->decode_xml(
+    $self->app->download( $self->repo->{url} . "/repodata/repomd.xml" ) );
+
+  my ($primary_file) =
+    grep { $_->{type} eq "primary" } @{ $repomd_ref->{data} };
+  $primary_file = $primary_file->{location}->[0]->{href};
+
+  my $url = $self->repo->{url} . "/" . $primary_file;
+  $self->app->logger->debug("Downloading $url.");
+  my $xml = $self->app->get_xml( $self->app->download_gzip($url) );
+
+  my @packages;
+  my @xml_packages = $xml->getElementsByTagName('package');
+  for my $xml_package (@xml_packages) {
+    my ($name_node)     = $xml_package->getChildrenByTagName("name");
+    my ($checksum_node) = $xml_package->getChildrenByTagName("checksum");
+    my ($size_node)     = $xml_package->getChildrenByTagName("size");
+    my ($location_node) = $xml_package->getChildrenByTagName("location");
+    push @packages,
+      {
+      location => $location_node->getAttribute("href"),
+      name     => $name_node->textContent,
+      checksum => {
+        type => $checksum_node->getAttribute("type"),
+        data => $checksum_node->textContent,
+      },
+      size => $size_node->getAttribute("archive"),
+      };
+  }
+
+  for my $package (@packages) {
+    my $package_url  = $self->repo->{url} . "/" . $package->{location};
+    my $package_name = $package->{name};
+
+    my $local_file = $self->repo->{local} . "/" . $package->{location};
+    $self->app->download_package(
+      url  => $package_url,
+      name => $package_name,
+      dest => $local_file,
+      cb   => sub {
+        $self->_checksum(
+          @_,
+          $package->{checksum}->{type},
+          $package->{checksum}->{data}
+        );
+      },
+      force => $option{update_files}
+    );
+  }
+
+  try {
+    $self->app->download_metadata(
+      url   => $self->repo->{url} . "/repodata/repomd.xml",
+      dest  => $self->repo->{local} . "/repodata/repomd.xml",
+      force => $option{update_metadata},
+    );
+
+    $self->app->download_metadata(
+      url   => $self->repo->{url} . "/repodata/repomd.xml.asc",
+      dest  => $self->repo->{local} . "/repodata/repomd.xml.asc",
+      force => $option{update_metadata},
+    );
+  }
+  catch {
+    $self->app->logger->error($_);
+  };
+
+  for my $file_data ( @{ $repomd_ref->{data} } ) {
+    my $file_url =
+      $self->{repo}->{url} . "/" . $file_data->{location}->[0]->{href};
+    my $file = basename $file_data->{location}->[0]->{href};
+
+    $self->app->download_metadata(
+      url  => $file_url,
+      dest => $self->repo->{local} . "/repodata/$file",
+      cb   => sub {
+        $self->_checksum(
+          @_,
+          $file_data->{checksum}->[0]->{type},
+          $file_data->{checksum}->[0]->{content}
+        );
+      },
+      force => $option{update_metadata},
+    );
+  }
+
+  if ( exists $self->repo->{images} && $self->repo->{images} eq "true" ) {
+    for my $file (
+      (
+        "images/boot.iso",           "images/efiboot.img",
+        "images/efidisk.img",        "images/install.img",
+        "images/pxeboot/initrd.img", "images/pxeboot/vmlinuz"
+      )
+      )
+    {
+      my $file_url   = $self->repo->{url} . "/" . $file;
+      my $local_file = $self->repo->{local} . "/" . $file;
+      $self->app->download_package(
+        url  => $file_url,
+        name => $file,
+        dest => $local_file,
+      );
+    }
+  }
+}
+
+sub init {
+  my $self = shift;
+
+  my $repo_dir = $self->app->get_repo_dir( repo => $self->repo->{name} );
+  mkpath "$repo_dir/repodata";
+
+  $self->_run_createrepo();
+}
+
+sub add_file {
+  my $self   = shift;
+  my %option = validate(
+    @_,
+    {
+      file => {
+        type => SCALAR
+      },
+    }
+  );
+
+  my $dest = $self->app->get_repo_dir( repo => $self->repo->{name} ) . "/"
+    . basename( $option{file} );
+
+  $self->app->add_file_to_repo( source => $option{file}, dest => $dest );
+
+  $self->_run_createrepo();
+}
+
+sub remove_file {
+  my $self = shift;
+
+  my %option = validate(
+    @_,
+    {
+      file => {
+        type => SCALAR
+      },
+    }
+  );
+
+  my $file = $self->app->get_repo_dir( repo => $self->repo->{name} ) . "/"
+    . basename( $option{file} );
+
+  $self->app->remove_file_from_repo( file => $file );
+
+  $self->_run_createrepo();
+}
+
+sub _run_createrepo {
+  my $self = shift;
+
+  my $repo_dir = $self->app->get_repo_dir( repo => $self->repo->{name} );
+  system "cd $repo_dir ; createrepo .";
+}
+
+sub _checksum {
+  my ( $self, $file, $type, $wanted_checksum ) = @_;
+
+  my $c_type = 1;
+  if ( $type eq "sha256" ) {
+    $c_type = "256";
+  }
+
+  my $sha = Digest::SHA->new($c_type);
+  $sha->addfile($file);
+  my $file_checksum = $sha->hexdigest;
+
+  $self->app->logger->debug(
+    "wanted_checksum: $wanted_checksum == $file_checksum");
+
+  if ( $wanted_checksum ne $file_checksum ) {
+    $self->app->logger->error("Checksum for $file wrong.");
+    confess "Checksum of $file wrong.";
+  }
+}
+
+1;
